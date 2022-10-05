1484d5ba7f3056c97753c8c7792377b2853e8888
pixelpercebe <83479307+pixelpercebe@users.noreply.github.com>
2022-10-05 17:34:49 +0200

    ejercicio 1.b

--- proyectos de Ada/AdaLabs_2.0/ProgLab3/ejercicio1/buscar_multiplo.adb
+++ proyectos de Ada/AdaLabs_2.0/ProgLab3/ejercicio1/buscar_multiplo.adb
@@ -4,14 +4,27 @@ use ada.text_IO, ada.integer_text_IO;
 
 
 procedure buscar_multiplos is
+
   num, multiplo :integer;
-  multiplo : boolean := false;
+  --es_multiplo : boolean := false;
 
 begin
 
-  while not multiplo and then not End_Of_File loop
-
+  put("Introduce un numero para buscar sus multiplos: ");
+  get(num);
+  new_line;
+  Put("Introduce mas numeros para buscar su multiplo: ");
+  get(multiplo);
+  
+  while multiplo mod num /= 0 and then not End_Of_File loop
+    get(multiplo);
   end loop;
+
+  if (multiplo mod num = 0) then 
+    put("Existe un multiplo en la secuencia");
+  else
+    put("NO existe multipllo en la secuencia");
+  end if;
 
 
 end buscar_multiplos;
