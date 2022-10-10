d7c9d455d5383bda57597e9e12f57eab1aec9754
IvanCB <icalbo360@gmail.com>
2022-10-10 03:04:06 +0200

    Empezando lab4

---
+++ proyectos de Ada/AdaLabs_2.0/ProgLab4/contar_divisores_propios.adb
@@ -0,0 +1,18 @@
+with ada.text_IO,ada.integer_text_IO;
+use ada.text_IO,ada.integer_text_IO;
+
+function contar_divisores_propios (num: in integer) return integer is 
+
+cont : integer := 0;
+
+begin
+
+for i in 2..num-1 loop
+  if num mod i = 0 then
+    cont := cont + 1;
+  end if;
+end loop;
+
+return cont;
+
+end contar_divisores_propios;
---
+++ proyectos de Ada/AdaLabs_2.0/ProgLab4/contar_divisores_propios_prueba.adb
@@ -0,0 +1,23 @@
+with ada.text_IO,ada.integer_text_IO, contar_divisores_propios;
+use ada.text_IO,ada.integer_text_IO;
+
+procedure contar_divisores_propios_prueba is
+
+--ESPECIFICACION
+--
+--
+--
+--
+
+begin
+
+put(contar_divisores_propios(0),0);
+
+
+
+end contar_divisores_propios_prueba;
+
+--CASOS DE PRUEBA
+--
+--
+--
