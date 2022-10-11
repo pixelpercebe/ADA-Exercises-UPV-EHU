88053067375e229ff24e0d9775731e228f0b3351
IvanCB <icalbo360@gmail.com>
2022-10-11 12:03:27 +0200

    carpeta digitos

---
+++ proyectos de Ada/AdaLabs_2.0/ProgLab4/digitos/contar_digitos.adb
@@ -0,0 +1,8 @@
+
+function contar_digitos (num : integer) return integer is
+
+begin
+
+return num'img(2..num'img'length)'length;
+
+end contar_digitos;
---
+++ proyectos de Ada/AdaLabs_2.0/ProgLab4/digitos/contar_digitos_prueba.adb
@@ -0,0 +1,11 @@
+with ada.integer_text_IO, ada.text_IO,contar_digitos;
+use ada.integer_text_IO,ada.text_IO;
+
+procedure contar_digitos_prueba is
+
+begin
+
+put(contar_digitos(14565),0);
+
+
+end contar_digitos_prueba;
