c6a63d6765c64fc23fc14fde7ca8117da9c419f5
IvanCB <icalbo360@gmail.com>
2022-10-13 11:26:44 +0200

    lab terminado

--- proyectos de Ada/AdaLabs_2.0/ProgLab4/digitos/esta_digito_prueba.adb
+++ proyectos de Ada/AdaLabs_2.0/ProgLab4/digitos/esta_digito_prueba.adb
@@ -6,12 +6,17 @@ procedure esta_digito_prueba is
 
 begin
 
-  put(esta_digito(1513,3)'img);
+  put_line(esta_digito(3,3)'img); --caso1
+  put_line(esta_digito(876,8)'img); --caso2
+  put_line(esta_digito(62184,1)'img); --caso3
+  put_line(esta_digito(11245,5)'img); --caso4
+  put_line(esta_digito(0982,3)'img); --caso5
 
 end esta_digito_prueba;
 
 --casos de prueba:
---caso1
---caso2
---caso3
---caso4
+--caso1 (el unico difgito es el buscado) --> 3,3 = true 
+--caso2 (el digito esta al principio) --> 876,8 = true 
+--caso3 (el digito esta en el centro) --> 62184,1 = true
+--caso4 (el digito esta al final) --> 11245,5 = true
+--caso5 (el digito no esta) --> 0982,3 = false
--- proyectos de Ada/AdaLabs_2.0/ProgLab4/digitos/que_digito_izquierda_prueba.adb
+++ proyectos de Ada/AdaLabs_2.0/ProgLab4/digitos/que_digito_izquierda_prueba.adb
@@ -5,15 +5,18 @@ procedure que_digito_izquierda_prueba is
 
 begin
 
-  put(que_digito_izquierda(23456,1),0);
-  put(que_digito_izquierda(23456,1),0);
-  put(que_digito_izquierda(23456,1),0);
-  put(que_digito_izquierda(23456,1),0);
+  put(que_digito_izquierda(5,1),0); --caso1
+  new_line;
+  put(que_digito_izquierda(7894,1),0); --caso2
+  new_line;
+  put(que_digito_izquierda(23456,3),0); --caso3
+  new_line;
+  put(que_digito_izquierda(91556,5),0); --caso4
 
 end que_digito_izquierda_prueba;
 
 --casos de prueba:
 --caso1 (un solo numero) -->  5 y 1 = 5
---caso2 (el numeoro de la izq) --> 7894 y 1 = 7 
---caso3 (el numero izq la der) --> 
---caso4 (el numero del la izq) -->
+--caso2 (el numeoro de la izquierda) --> 7894 y 1 = 7 
+--caso3 (el numero del centro) -->  23456,3 = 4
+--caso4 (el numero del la derecha) --> 91556,5 = 6
