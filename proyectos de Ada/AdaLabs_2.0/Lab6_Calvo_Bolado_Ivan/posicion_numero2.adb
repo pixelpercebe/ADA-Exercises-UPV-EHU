bbd92aabee0eb34de573a7ea5bbd5ac0421aed61
pixelpercebe <83479307+pixelpercebe@users.noreply.github.com>
2022-11-03 20:55:30 +0100

    EJER2

--- proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/posicion_numero2.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/posicion_numero2.adb
@@ -1,4 +1,5 @@
-with Tipos_lab6;use Tipos_lab6;
+with Tipos_lab6,ada.Integer_text_IO, ada.text_IO;
+use Tipos_lab6,ada.Integer_text_IO,ada.text_IO;
 
 procedure Posicion_Numero2 (N: T_Valores_Max; Imagen : T_Matriz2;
 I, J: out Natural) is
@@ -12,8 +13,11 @@ I := Imagen'first;
 J := Imagen(1)'first;
 
 loop
-  J := 0;
+  J := 1;
+  PUT(i);
+  new_line;
   while J < Imagen(I)'last and then Imagen(I)(J) /= N loop
+   put(Imagen(I)(j));
     J := J + 1;
   end loop;
   exit when Imagen(I)(J) = N or else I=Imagen'last;
