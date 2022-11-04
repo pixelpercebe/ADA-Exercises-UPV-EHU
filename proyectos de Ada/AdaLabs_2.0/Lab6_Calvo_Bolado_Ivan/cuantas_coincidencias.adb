87f6b016fa8e607fcbe3cc3d908ac73bcbc570a2
pixelpercebe <83479307+pixelpercebe@users.noreply.github.com>
2022-11-04 16:50:17 +0100

    ejer3 pruebas

--- proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/cuantas_coincidencias.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/cuantas_coincidencias.adb
@@ -12,7 +12,7 @@ begin
   I := Imagen1'first(1);
   cuantas := 0;
   for I in Imagen1'range(1) loop
-    J := Imagen2'first(2);
+    J := Imagen1'first(2);
     for J in Imagen1'range(2) loop
       cuantas := cuantas + Boolean'pos(Imagen1(I,J)= Imagen2(I)(J));
     end loop;
--- proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/cuantas_coincidencias_prueba.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/cuantas_coincidencias_prueba.adb
@@ -1,13 +1,19 @@
-with ada.Integer_text_IO,tipos_lab6,Cuantas_Coincidencias;
-use ada.Integer_text_IO,tipos_lab6;
+with ada.Integer_text_IO,tipos_lab6,Cuantas_Coincidencias,ada.text_IO;
+use ada.Integer_text_IO,tipos_lab6,ada.text_IO;
 
 procedure Cuantas_Coincidencias_prueba is
 
-no_hay1 : T_Matriz1(4..6,) := 
-no_hay2 : T_Matriz2 :=
+no_hay1 : T_Matriz1(4..5,1..5) := ((201,202,203,204,205),(206,207,208,209,210));
+no_hay2 : T_Matriz2 (4..5):= (4 =>(1,2,3,4,5),5=>(6,7,8,9,10));
+extremos1 : T_Matriz1 (20..21,1..5) :=
+extremos2 : T_Matriz2 (20..21) :=
+coincidencias : integer;
+
 
 begin
 
+Cuantas_Coincidencias(no_hay1,no_hay2,coincidencias);
+put(coincidencias,0);
 
 
 end Cuantas_Coincidencias_prueba;
