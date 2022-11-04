bea0e9490ae528b657dbf5fb741e4b79a03c81b2
pixelpercebe <83479307+pixelpercebe@users.noreply.github.com>
2022-11-04 19:38:21 +0100

    casos de prueba

--- proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/cuantas_coincidencias_prueba.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/cuantas_coincidencias_prueba.adb
@@ -5,8 +5,10 @@ procedure Cuantas_Coincidencias_prueba is
 
 no_hay1 : T_Matriz1(4..5,1..5) := ((201,202,203,204,205),(206,207,208,209,210));
 no_hay2 : T_Matriz2 (4..5):= (4 =>(1,2,3,4,5),5=>(6,7,8,9,10));
-extremos1 : T_Matriz1 (20..21,1..5) :=
-extremos2 : T_Matriz2 (20..21) :=
+extremos1 : T_Matriz1 (20..21,1..5) := ((54,35,2,4,7),(89,0,255,245,255));
+extremos2 : T_Matriz2 (20..21) := (20 => (54,83,82,3,7),21 =>(89,8,32,1,255));
+todos1 : T_Matriz1 (2..4,1..5) := ((others => 255),(others => 0),(others => 255)); 
+todos2 : T_Matriz2 (2..4) := (2 => (others => 255),3=>(others => 0),4=>(others => 255));
 coincidencias : integer;
 
 
@@ -14,6 +16,15 @@ begin
 
 Cuantas_Coincidencias(no_hay1,no_hay2,coincidencias);
 put(coincidencias,0);
-
-
+new_line;
+Cuantas_Coincidencias(extremos1,extremos2,coincidencias);
+put(coincidencias,0);
+new_line;
+Cuantas_Coincidencias(todos1,todos2,coincidencias);
+put(coincidencias,0);
 end Cuantas_Coincidencias_prueba;
+
+--Casos de prueba
+--caso1 (Ningun elemento el comun) -> no_hay1 y no_hay2 = 0
+--caso2 (Elementos en comun en los extremos de los arrays) -> extremos1 y extremos2 = 4
+--caso3 (todos los elemmentos en comun) -> todos1 y todos2 = 
--- proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/obtener_media1_prueba.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab6_Calvo_Bolado_Ivan/obtener_media1_prueba.adb
@@ -1,4 +1,4 @@
-with ada.float_text_IO, Tipos_Lab6, obtener_media1; use ada.float_text_IO,Tipos_Lab6;
+with ada.float_text_IO, Tipos_Lab6, ada.text_IO, obtener_media1; use ada.float_text_IO,Tipos_Lab6, ada.text_IO;
 
 
 procedure obtener_media1_prueba is
@@ -14,3 +14,7 @@ new_line;
 Put(obtener_media1(no_cuadrada),0,2,0);
 
 end obtener_media1_prueba;
+
+--casos dde pueba
+--caso1 (una matriz cuadrada) -> ((1,2,3),(5,6,7),(8,9,10)) = 5.67
+--caso2 (una matriz no cuadrada) -> ((1,2,3,4,5),(6,7,8,9,10),(11,12,13,14,15)) = 8
