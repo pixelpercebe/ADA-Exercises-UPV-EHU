d629488513d7c823b847a9162f0d6f1d68787932
pixelpercebe <83479307+pixelpercebe@users.noreply.github.com>
2022-11-14 11:50:44 +0100

    prueba ejer1

--- proyectos de Ada/AdaLabs_2.0/Lab8_Calvo_Bolado_Ivan/numeros/ejecutar_lista_numeros.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab8_Calvo_Bolado_Ivan/numeros/ejecutar_lista_numeros.adb
@@ -1,12 +1,13 @@
-with lista_numeros, ada.Integer_Text_IO; use lista_numeros, ada.Text_IO;
+with lista_numeros, ada.Text_IO; use lista_numeros, ada.Text_IO;
 
 
 procedure ejecutar_lista_numeros is
 
-L,M,N : T_Lista_Enteros;
+L: T_Lista;
 
 begin
+cargar_registros(L);
 
-
+Put(esta(L,24)'img);
 
 end ejecutar_lista_numeros;
--- proyectos de Ada/AdaLabs_2.0/Lab8_Calvo_Bolado_Ivan/numeros/lista_numeros.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab8_Calvo_Bolado_Ivan/numeros/lista_numeros.adb
@@ -1,21 +1,30 @@
 
 Package body Lista_numeros is 
 
+Procedure cargar_registros (L: IN OUT T_Lista) IS
+
+begin
+  L.numeros(1..6) := (24,16,2,13,5,17);
+  L.cont:=6;
+END cargar_registros;
+
+
+
 
 function esta (L:t_lista; N:integer) return boolean is
-  i : integer := L.lista'first;
+  i : integer := L.numeros'first;
 begin
-  while i < L.cont and L.lista(i) /= N loop
+  while i < L.cont and L.numeros(i) /= N loop
     i := i + 1;
   end loop;
-  return L.lista(i) /= N;
+  return L.numeros(i) = N;
 end esta;
 
 
 Procedure Elimina_I (L: in out t_lista; I:integer) is
 begin
 
-L.lista(I..L.cont-1) := L.lista(I+1..l.cont);
+L.numeros(I..L.cont-1) := L.numeros(I+1..l.cont);
 L.cont := L.cont - 1;
 
 end Elimina_I;
