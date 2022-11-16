92e2d44f50d8ce64600b696971827f7b27a09340
pixelpercebe <83479307+pixelpercebe@users.noreply.github.com>
2022-11-16 20:14:23 +0100

    giro_I completado

--- proyectos de Ada/AdaLabs_2.0/Lab8_Calvo_Bolado_Ivan/caracteres/palabras.adb
+++ proyectos de Ada/AdaLabs_2.0/Lab8_Calvo_Bolado_Ivan/caracteres/palabras.adb
@@ -9,9 +9,12 @@ package body palabras is
   end escribe_Palabra;
 
   procedure Giro_I(L:in out t_lista;i:integer) is
-    Aux:String
+    Aux:String(1..max);
   begin
-
+    L.letras(l.cont-I..l.cont) = Aux(1..i);
+    aux(i..max):=(other=>' ');
+    L.letras(I..l.cont)=L.letras(l.cont-I..l.cont);
+    l.letras(1..I)=aux(1..i);
 
 
   end Giro_I;
